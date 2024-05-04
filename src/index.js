0cfa3b26b6960590afa2a982f9c207f2904f9deb
NIKHIL SHARMA <nikhilsharma302@gmail.com>
2024-05-04 20:49:47 +0530

    code added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
+  await fs.writeFile(filename,fileContent)
 };
 
 module.exports = { writeFile };
